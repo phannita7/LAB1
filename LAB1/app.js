84e35305d93590aea7ab8a158aaa77b062e28e91
KING <Wootthinan.p@ku.th>
2020-03-19 23:31:56 +0700

    Add file Lab1

---
+++ LAB1/app.js
@@ -0,0 +1,102 @@
+// const express = require('express')
+// const app = express()
+
+
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+app.get('/',(seq, res) => res.send('Hello Boy'))
+
+app.listen(3000, () => console.log('Example app listening on post 3000!'))
+
+app.use(bodyParser.json());
+const users = [
+    {
+        "id":1,
+        "name":"KING",
+        "email":"Wootthinan.p@ku.th"
+    },
+    {
+        "id":2,
+        "name":"BEE",
+        "email":"Jirarat.h@ku.th"
+    },
+    {
+        "id":3,
+        "name":"Q",
+        "email":"Chirayuth.sa@ku.th"
+    }
+]
+app.get('/api/users',(req, res) => {
+    const result = {
+        "status":200,
+        "data":users
+    }
+    return res.json(result)
+})
+app.get("/api/user/:id",(req,res) => {
+    let user = users.find(user => user.id === parseInt(req.params.id));
+    if (!user)
+        return res
+            .status(400)
+            .json({ status: 400, message: "Not found user with the given ID"});
+    res.user = user;
+    const result = {
+        status: 200,
+        data: res.user
+    };
+    return res.json(result);
+});
+app.post('/api/users', (req, res) => {
+    let user = {
+        "id":users.length+1,
+        "name":req.body.name,
+        "email":req.body.email
+    }
+    users.push(user)
+    const result = {
+        "status":200,
+        "data":users
+    }
+    return res.json(result)
+})
+
+app.put('/api/user/:id', (req, res) => {
+    let user = users.find ( (user) => user.id === parseInt(req.params.id))
+    if (!user)
+        return res
+            .status(400)
+            .json({ status: 400, message: "Not found user with the given ID" });
+    let user_index = users.findIndex( (user) => user.id === parseInt(req.params.id))
+
+    user = {
+        "id":user.id,
+        "name":req.body.name,
+        "email":req.body.email
+    }
+
+    users[user_index] = user
+
+    const result = {
+        "status":200,
+        "data":user
+    }
+    return res.json(result)
+})
+
+app.delete("/api/user/:id", (req, res) => {
+    let user = users.find(user => user.id === parseInt(req.params.id));
+    if (!user)
+        return res
+            .status(400)
+            .json({ status: 400, message: "Not found user with the given ID" });
+
+    let user_index = users.findIndex(user => user.id === parseInt(req.params.id));
+    users.pop(user_index);
+
+    const result = {
+        "status":200,
+        "data":users
+    };
+    return res.json(result);
+});
